ef99559ee091287e14935d743156fb0d21ef67dd
rod773 <rodrigoarcadio1@gmail.com>
2024-08-03 19:32:52 +0200

    update

---
+++ src/components/BannerSlider/BannerSlider.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Fade from "react-reveal/Fade";
+
+const BannerSlider = () => {
+  return <div>BannerSlider</div>;
+};
+
+export default BannerSlider;
