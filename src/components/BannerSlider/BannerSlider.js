bd184ed648c7c1af35a7c2ef77ce6d1630906f0b
rod773 <rodrigoarcadio1@gmail.com>
2024-08-06 15:51:34 +0200

    update

--- src/components/BannerSlider/BannerSlider.js
+++ src/components/BannerSlider/BannerSlider.js
@@ -7,10 +7,22 @@ import slide2 from "assets/img/slide-img-2.jpg";
 
 const SingleCarousel1 = styled.div`
   background-image: url(${slide1});
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  position: relative;
+  padding: 200px 0;
+  z-index: 1;
 `;
 
 const SingleCarousel2 = styled.div`
   background-image: url(${slide2});
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  position: relative;
+  padding: 200px 0;
+  z-index: 1;
 `;
 
 const BannerSlider = () => {
@@ -25,7 +37,7 @@ const BannerSlider = () => {
                 <Carousel>
                   <Carousel.Item>
                     {/* <!-- Banner Single Carousel Start --> */}
-                    <SingleCarousel1 className="single-carousel-wrap slide-item-1">
+                    <SingleCarousel1>
                       <div className="banner-caption text-center text-lg-left">
                         <h4>Rubby Store</h4>
                         <h2>Ring Solitaire Princess</h2>
@@ -43,7 +55,7 @@ const BannerSlider = () => {
 
                   <!-- Banner Single Carousel Start --> */}
                   <Carousel.Item>
-                    <SingleCarousel2 className="single-carousel-wrap slide-item-2">
+                    <SingleCarousel2>
                       <div className="banner-caption text-center text-lg-left">
                         <h4>New Collection 2017</h4>
                         <h2>Beautiful Earrings</h2>
