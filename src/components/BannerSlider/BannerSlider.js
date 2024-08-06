3f81883cfa40e73315da9c7ee5467f2cb3a9a7c8
rod773 <rodrigoarcadio1@gmail.com>
2024-08-06 14:06:08 +0200

    update

--- src/components/BannerSlider/BannerSlider.js
+++ src/components/BannerSlider/BannerSlider.js
@@ -3,7 +3,55 @@ import Carousel from "react-bootstrap/Carousel";
 import Fade from "react-reveal/Fade";
 
 const BannerSlider = () => {
-  return <div>BannerSlider</div>;
+  return (
+    <>
+      {/* <!--== Banner // Slider Area Start ==--> */}
+      <section id="banner-area">
+        <Carousel>
+          <div className="ruby-container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div id="banner-carousel" className="owl-carousel">
+                  {/* <!-- Banner Single Carousel Start --> */}
+                  <div className="single-carousel-wrap slide-item-1">
+                    <div className="banner-caption text-center text-lg-left">
+                      <h4>Rubby Store</h4>
+                      <h2>Ring Solitaire Princess</h2>
+                      <p>
+                        Eodem modo typi, qui nunc nobis videntur parum clari,
+                        fiant sollemnes in futurum.
+                      </p>
+                      <a href="#" className="btn-long-arrow">
+                        Shop Now
+                      </a>
+                    </div>
+                  </div>
+                  {/* <!-- Banner Single Carousel End -->
+
+                    <!-- Banner Single Carousel Start --> */}
+                  <div className="single-carousel-wrap slide-item-2">
+                    <div className="banner-caption text-center text-lg-left">
+                      <h4>New Collection 2017</h4>
+                      <h2>Beautiful Earrings</h2>
+                      <p>
+                        Eodem modo typi, qui nunc nobis videntur parum clari,
+                        fiant sollemnes in futurum.
+                      </p>
+                      <a href="#" className="btn-long-arrow">
+                        Shop Now
+                      </a>
+                    </div>
+                  </div>
+                  {/* <!-- Banner Single Carousel End --> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel>
+      </section>
+      {/* <!--== Banner Slider End ==--> */}
+    </>
+  );
 };
 
 export default BannerSlider;
