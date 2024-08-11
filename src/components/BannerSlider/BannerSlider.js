d5d5bd1653a58af29bf30f6ef610c97749778335
rod773 <rodrigoarcadio1@gmail.com>
2024-08-11 16:18:31 +0200

    update

--- src/components/BannerSlider/BannerSlider.js
+++ src/components/BannerSlider/BannerSlider.js
@@ -27,7 +27,7 @@ const BannerSlider = () => {
                     {/* <!-- Banner Single Carousel Start --> */}
                     <SingleCarousel1 className="single-carousel-wrap slide-item-1">
                       <div className="banner-caption text-center text-lg-left">
-                        <Fade direction="up">
+                        <Fade direction="up" duration="1000">
                           <h4>Rubby Store</h4>
                           <h2>Ring Solitaire Princess</h2>
                           <p>
@@ -47,7 +47,7 @@ const BannerSlider = () => {
                   <Carousel.Item>
                     <SingleCarousel2 className="single-carousel-wrap slide-item-2">
                       <div className="banner-caption text-center text-lg-left">
-                        <Fade direction="up">
+                        <Fade direction="up" duration="1000">
                           <h4>New Collection 2017</h4>
                           <h2>Beautiful Earrings</h2>
                           <p>
